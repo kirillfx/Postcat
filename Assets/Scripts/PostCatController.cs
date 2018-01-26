a68718df167e43c67db2eab315ab8927e79db6d3
Kirill Chayka <kirillfx@gmail.com>
2018-01-26 21:29:27 +0200

    init

---
+++ Assets/Scripts/PostCatController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostCatController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	Rigidbody2D rb;
+
+
+	void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		
+		// rb.MovePosition(new Vector3(h,v, 0.0f) * speed);
+		rb.MovePosition(rb.transform.position + 
+			new Vector3(h, v, 0.0f) * speed * Time.fixedDeltaTime);
+	}
+}
