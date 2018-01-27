28c26fccdff87aa3cbe93803695b1275400606a2
MariaIvanus <35850126+MariaIvanus@users.noreply.github.com>
2018-01-28 00:12:46 +0200

    Add files via upload

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PlaySound()
+    {
+        //TODO: добавить задрежку для проигрывания звука слайдера - зайдает
+        FindObjectOfType<AudioManager>().Play("btn");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    
+}
