7df6bae443a4d7c6de0ff1b146748b1258a53393
Kirill Chayka <kirillfx@gmail.com>
2018-01-27 14:19:48 +0200

    PolyObstacle added.

---
+++ Assets/Scripts/PolyObstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PolyObstacle : MonoBehaviour {
+
+	public Transform[] choices;
+
+	void Awake() {
+		
+		int index = 
+			Mathf.FloorToInt(Random.RandomRange(0, choices.Length + 1));
+		
+		Transform obj = choices[index];
+
+		Instantiate(obj, transform.position, obj.rotation);
+
+	}
+
+}
