8c72ffabe63abf4f0472c78b7e4b8688b76127bf
Kirill Chayka <kirillfx@gmail.com>
2018-01-31 18:14:50 +0200

    init

---
+++ Assets/RootController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootController : MonoBehaviour {
+
+	public Transform target;
+	public float max;
+	private Animation anim;
+
+
+	void Awake() {
+		anim = GetComponent<Animation>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float current = Mathf.Max(target.position.y, max) / max;
+
+	}
+}
